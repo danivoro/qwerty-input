c8ff10118b34a2ed371c43cb197123e1d0313927
dani voro <mr.voro@gmail.com>
2023-07-09 13:15:00 +0200

    finishing the exercise

--- src/components/WordDescription.tsx
+++ src/components/WordDescription.tsx
@@ -8,12 +8,14 @@ function WordDescription({ wordToDescribe }: WordDescriptionProps): JSX.Element 
   const hasAtLeastFiveCharacters = wordToDescribe.length > 5;
   const containsAVowel = hasAVowel(wordToDescribe);
   const hasTheLetterQ = wordToDescribe.toLowerCase().match('q');
+  const containsSpecialCharacter = wordToDescribe.match(/[!?£]/)
 
   return (
     <ul>
       {hasAtLeastFiveCharacters && <li>Has at least five characters</li>}
       {containsAVowel && <li>Contains a vowel</li>}
       {hasTheLetterQ && <li>Contains the letter Q</li>}
+      {containsSpecialCharacter && <li>Contains special character</li>}
     </ul>
   )
 }
